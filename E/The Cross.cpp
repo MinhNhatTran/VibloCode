d7134fc5bb2db4652aa761a7bdfd148aecf172cd
Tran Minh Nhat <trminhnhat99@gmail.com>
2020-01-03 10:56:27 +0700

    Update The Cross.cpp

--- E/The Cross.cpp
+++ E/The Cross.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <algorithm>
-#include <stack>
-#include <utility>
 
 using namespace std;
 
@@ -14,29 +11,18 @@ int main() {
 			cin >> a[i][j];
 
 	// check ben trong
-	for(int i = 1; i < n - 1; ++i) {
-		for(int j = 1; j < n - 1; ++j) {
-			if(a[i][j] == '#') {
-				if (a[i - 1][j] == a[i + 1][j] && 
-					a[i + 1][j] == a[i][j - 1] &&
-					a[i][j - 1] == a[i][j + 1] &&
-					a[i][j + 1] == a[i][j]) {
-          
+	for(int i = 1; i < n - 1; ++i)
+		for(int j = 1; j < n - 1; ++j)
+			if(a[i][j] == '#')
+				if ( (a[i - 1][j] == a[i + 1][j]) && (a[i + 1][j] == a[i][j - 1]) && (a[i][j - 1] == a[i][j + 1]) && (a[i][j + 1] == a[i][j]) )
 					a[i - 1][j] = a[i + 1][j] = a[i][j - 1] = a[i][j + 1] = a[i][j] = '*';
 
-				}
-			}
-		}
-	}
-
-	for(int i = 0; i < n; ++i) {
-		for(int j = 0; j < n; ++j) {
+	for(int i = 0; i < n; ++i)
+		for(int j = 0; j < n; ++j)
 			if(a[i][j] == '#') {
 				cout << "NO" << endl;
 				return 0;
 			}
-		}
-	}
 	cout << "YES" << endl;
 
 	return 0;
